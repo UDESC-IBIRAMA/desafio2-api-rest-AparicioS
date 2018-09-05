b361640eabbd980db0a3028fc945d7bbe95cb373
AparicioS <31492016+AparicioS@users.noreply.github.com>
2018-09-04 22:44:49 -0300

    inicio

---
+++ desafio/target/generated-sources/annotations/udesc/desafio/veiculo_.java
@@ -0,0 +1,18 @@
+package udesc.desafio;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-04T21:54:29")
+@StaticMetamodel(veiculo.class)
+public class veiculo_ { 
+
+    public static volatile SingularAttribute<veiculo, String> motor;
+    public static volatile SingularAttribute<veiculo, String> quilometro;
+    public static volatile SingularAttribute<veiculo, String> cor;
+    public static volatile SingularAttribute<veiculo, String> montadora;
+    public static volatile SingularAttribute<veiculo, Long> id;
+    public static volatile SingularAttribute<veiculo, String> modelo;
+
+}
